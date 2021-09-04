dd2d8ebb41a0431c9950da3b7b8f73d6e033f741
emreYoleri-gitHub <emreyoleri01@hotmail.com>
2021-09-04 13:24:25 +0300

    It prepared post model and post model functions. tested

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
-import express from "express"
-import bodyParser from "body-parser"
-import Mongoose from "mongoose"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import postRouter from "./routers/postRouter.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json({ limit: "20mb" }));
+
+app.use("/posts", postRouter);
+
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("conntected to db"))
+    .catch((err) => console.log(err));
+});
